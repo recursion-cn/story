598831bd71a651bef13192a4fec853c25cfc8314
lncwwn <lncwwn@gmail.com>
2015-11-06 17:49:56 +0800

    refine

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,9 @@
 'use strict'
 
 require('expose?$!expose?jQuery!jquery');
+
+$('body').on('mouseover', '.nav-user-panel', function() {
+    $(this).find('.user-panel').fadeIn();
+}).on('mouseleave', '.nav-user-panel', function() {
+    $(this).find('.user-panel').fadeOut();
+});
